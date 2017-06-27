26b63b714438ceb67863df458c48018b45cab1f4
pdhoward <patrick.howard@hotmail.com>
2017-06-27 17:06:43 -0400

    updates

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -9,25 +9,38 @@ const config = {
     path: __dirname,
     filename: 'bundle.js',
   },
+
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      test: /\.(js|jsx)$/,
-      loader: 'babel',
-    },
+
+    rules: [
+
       {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract('css!sass'),
-      }],
+      test: /\.(js|jsx)$/,
+      include: [
+          /src/ ],
+      loader: 'babel-loader',
+      options: {
+          presets: ["es2015"]
+        }
+      },
+
+      {
+      test: /\.scss$/,
+        use: [{
+          loader: ExtractTextPlugin.extract('css!sass'),
+        }]
+      }
+  ],
   },
   devServer: {
     historyApiFallback: true,
     contentBase: './',
   },
+  // This lets us debug our react code in chrome dev tools. Errors will have lines and file names
+  // Without this the console says all errors are coming from just coming from bundle.js
+  devtool: "eval-source-map",
   plugins: [
     new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } }),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({
       compress: { warnings: false },
       output: { comments: false },
@@ -36,9 +49,7 @@ const config = {
       minimize: true,
       mangle: { except: ['$super', '$', 'exports', 'require', '$q', '$ocLazyLoad'] },
     }),
-    new ExtractTextPlugin('src/public/stylesheets/app.css', {
-      allChunks: true,
-    }),
+    new ExtractTextPlugin({ filename: 'src/public/stylesheets/app.css', allChunks: true }),
   ],
 };
 
